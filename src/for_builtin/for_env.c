415a8e139a8b4782b3a3698810482936a1e653c7
Hunjin Choi <huchoi@c4r3s6.42seoul.kr>
2021-04-11 02:04:21 +0900

    change

---
+++ src/for_builtin/for_env.c
@@ -0,0 +1,98 @@
+#include "../minishell.h"
+
+int     match_key(char *key_value, char *str)//검증필요
+{
+    int size = 0;
+    while (key_value[size] != 0 && key_value[size] != '=')
+        size++;
+    if (!ft_strncmp(key, str, size - 1) && (str[size] == '=' || str[size] == '\0'))
+        return (TRUE);
+    return (FALSE);
+}
+
+char 
+/*                                               */
+/*                                               */
+/*                                               */
+/*                                               */
+
+void    get_value(char *str, char *buf)
+{
+    int idx = 0;
+    char *p;
+    p = ft_strchr(str, '=');
+    ft_strcpy(buf, p + 1);
+}
+
+int     findenv(t_data *data, char *buf)
+{
+    int idx = 0;
+    char *str;
+    char *key = data->cmd[1];
+    while (data->env[idx])
+    {
+        if (match_key(key, data->env[idx] == TRUE)
+        {
+            get_value(data->env[idx], buf);
+            return (1);
+        }
+    }
+    return (-1)
+}
+
+
+//for export, unset command
+void    renewer_env(char **env, char *key, char *str, int size)
+{
+    new_env = malloc(sizeof(char *) * size);
+    new_env[size - 1] = NULL;
+    int idx = 0;
+    int count = 0;
+    if (str == NULL)
+    {//delete one env
+        while (env[idx])
+        {
+            if (match_key(key, env[idx]) == TRUE)//not copy this key, value
+            {
+                free(env[idx]);
+                idx++;
+            }
+            else//copy this key, value
+                new_env[count++] = env[idx++];
+        }
+        new_env[count] = NULL;//size == count?
+    }
+    else
+    {//add one env
+        while (data->env[idx])
+            new_env[count++] = env[idx++];
+        new_env[count++] = ft_strdup(str);
+        new_env[count] = NULL;//size == count?
+    }
+    free(env);
+}
+
+int    delete_env(t_data *data, char *key)
+{
+    int idx = 0;
+    while (data->env[idx])
+        idx++;
+    renewer_env(data->env, key, NULL, idx + 1 - 1);
+    return (1)
+}
+
+int     add_env(t_data *data, char *key_value)
+{
+    int idx = 0;
+    while (data->env[idx])
+    {
+        if (match_key(key_value, data->env[idx]) == TRUE)
+        {
+            ft_putstr_fd("the key already exist", 1);
+            return (-1);
+        }
+    }
+    //renewer_env(data->env, NULL, data->cmd[1], idx + 1 + 1);
+    renewer_env(data->env, key_value, data->cmd[1], idx + 1 + 1);
+    return (1);
+}
--- src/process.c
+++ src/process.c
@@ -3,38 +3,70 @@
 #include <sys/wait.h>//for wait function
 #include "minishell.h"
 int check_apos(char *str);
-void	process_builtin(t_data *data)
+void	process_builtin(t_data *data)//fork()사용하면 절대 안됨.
 {
-	char *buf;
+	char buf[1024];
 	int idx;
-	getcwd(buf, 100);
-	chdir("/root");
-	if (!ft_strcmp(data.cmd[0], "echo", ft_strlen("echo") + 1))
+
+	if (!ft_strncmp(data.cmd[0], "echo", 5))
 	{
-		//write() printf() getenv() 
+		//write() printf() getenv()
 		//find_apos(char *str)
 		idx = 1;
-		while (cmd[idx])
+		if (!ft_strncmp(cmd[idx], "-n", 3))
 		{
-			ft_putstr_fd(cmd[0] + check_apos(cmd[0]), 1);
 			idx++;
 		}
+		while (cmd[idx])
+		{
+			ft_putstr_fd(cmd[0] + check_apos(cmd[0]), 1);//표준출력 리다이렉션은 영리치님의 몫인데, 명령어가 표준입력 리다이렉션이면?
+			idx++;
+		}
+		if (idx != 1)
+		{
+			//option_manage();
+		}
 	}
-	else if (!ft_strcmp(data.cmd[0], "pwd", ft_strlen("pwd") + 1))
+	else if (!ft_strncmp(data.cmd[0], "pwd", 4))
 	{
-
+		if (0 == getcwd(buf, 1024))
+			ft_putstr_fd("err", 1);
+		ft_putstr_fd(buf, 1);//표준출력 디라이렉션 처리는 영리치님의 몫임.
 	}
-	else if (!ft_strcmp(data.cmd[0], "env", ft_strlen("env") + 1))
+	else if (!ft_strncmp(data.cmd[0], "env", 4))//getenv()함수 쓰면 절대 안됨.
 	{
-
-	}
-	else
+		if (-1 == findenv(data, buf))
+			ft_putstr_fd("not fount that env", 1);
+		ft_putstr_fd(buf, 1);//표준출력 디라이렉션 처리는 영리치님의 몫임.
+	}//dup로 표준입력 출력 다른걸로 변경해주신걸 내가 사용하는데, 사용이후 원래값으로 변경해주는 작업이 필요. 이작업도 영리치님이...
+	else if (!ft_strncmp(data.cmd[0], "cd", 3))
 	{
-
+		if (-1 == chdir(data.cmd[1]))
+			ft_putstr_fd("err", 1);
 	}
+	else if (!ft_strncmp(data.cmd[0], "export", 7))
+	{
+		add_env(data, data.cmd[1])
+	}
+	else if (!ft_strncmp(data.cmd[0], "unset", 6))
+	{
+		delete_env(data, data,cmd[1])
+	}
+	else if (!ft_strncmp(data.cmd[0], "exit", 5))
+	{
+		//terminate shtell program
+	}
+	else if (!ft_strncmp(data.cmd[0], "bash", 5))
+	{
+		//아직 미정임
+	}
+	else 
+		ft_putstr_fd("error", 1);
 }
 
-void	process_bin(t_data *data)
+
+
+void	process_bin(t_data *p_data)
 {
 	int status;
 	pid_t pid;
