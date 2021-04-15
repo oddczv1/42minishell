feda36e747bde7e6eb229e228be81e7d29eb1e07
huchoi <nyz1058@nate.com>
2021-04-15 14:37:10 +0900

    colplete parse.c

--- src/think.c
+++ src/think.c
@@ -1,7 +1,6 @@
 #include "../minishell.h"
 #define CUR(X) ((X % 2) * (2))
 //idx짝수->0, idx홀수->2
-#define BEFORE
 int     pipe_func(t_data *data, int *fx, int fd, int idx)
 {
     pipe(&fx[CUR(idx)]);
@@ -33,8 +32,6 @@ int     pipe_func(t_data *data, int *fx, int fd, int idx)
 }
 void    process_pipe(t_data *d)
 {
-    pid_t pid;
-    int status;
     int fd = dup(0);
     int idx = 0;
     int fx[4];
@@ -46,36 +43,33 @@ void    process_pipe(t_data *d)
 		ft_remove_mark(d);							
 		ft_check_redirection(d);
         fd = pipe_func(d, fx, fd, idx);
-        printf("test : %d\n ", fd);
-        sleep(10);
         idx++;
     }
-    //d->cmd = ft_split(d->argv[idx], ' ');
-    if (0 == (pid = fork()))
-    {
-        dup2(fd, 0);//표준출력 리다이렉션은 하지않음.
-        close(fd);
-        close(fx[CUR(idx-1)]);
-        char *exec_file;
-        if (is_builtin(d))
-            process_builtin(d);//이 부분이 조금 애매하긴한데... 뭔가 확신이 안생김.
-        else if (is_exec_bin(d))
-        {   
-            exec_file = ft_strjoin("/bin/", d->cmd[0]); 
-            execve(exec_file, d->cmd, NULL);
-        }
-        else if (is_exec_usr(d))
-        {   
-            exec_file = ft_strjoin("/usr/bin/", d->cmd[0]); 
-            execve(exec_file, d->cmd, NULL);
-        }
-        else
-            ft_putstr_fd("error in piped...", 2);
+    ft_check_argv(d->argv[idx]);
+	d->cmd = ft_split_pipe(d->argv[idx]);
+	ft_check_env(d);
+	ft_remove_mark(d);							
+	ft_check_redirection(d);
+    dup2(fd, 0);
+    close(fd);
+    close(fx[CUR(idx-1)]);
+    //char *exec_file;
+    if (d->cmd[0][0] == 'l')
+        execve("/bin/ls", d->cmd, NULL);
+    else
+        execve("/usr/bin/grep", d->cmd, NULL);
+    /*if (is_builtin(d))
+        process_builtin(d);//이 부분이 조금 애매하긴한데... 뭔가 확신이 안생김.
+    else if (is_exec_bin(d))
+    {   
+        exec_file = ft_strjoin("/bin/", d->cmd[0]); 
+        execve(exec_file, d->cmd, NULL);
+    }
+    else if (is_exec_usr(d))
+    {   
+        exec_file = ft_strjoin("/usr/bin/", d->cmd[0]); 
+        execve(exec_file, d->cmd, NULL);
     }
     else
-    {
-        close(fd);
-        close(fx[CUR(idx-1)]);
-        waitpid(pid, &status, 0);
-    }
+        ft_putstr_fd("error in piped...", 2);*/
 }
