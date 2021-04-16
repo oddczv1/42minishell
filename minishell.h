69a4ef23f3171155063e402f7aa8f9c8e8153b52
huchoi <nyz1058@nate.com>
2021-04-16 12:55:04 +0900

    dandy minishell.h

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: huchoi <huchoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 14:56:36 by youngrch          #+#    #+#             */
-/*   Updated: 2021/04/15 14:41:23 by huchoi           ###   ########.fr       */
+/*   Updated: 2021/04/16 12:54:13 by huchoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,39 +55,39 @@ void				ft_remove_mark(t_data *d);
 void				ft_put_env_value(t_data *d, char *str, int *end, int start, int brac);
 void				ft_put_env(t_data *d, char *str, int *i);
 
-void	delete_last_newline(char **cmd);
-int     match_key(char *key_value, char *str);
-void    get_value(char *str, char *buf);
-int     findenv(t_data *data, char *buf);
-void    renewer_env(t_data *data, char *key, char *str, int size);
-int    delete_env(t_data *data, char *key);
-int     add_env(t_data *data, char *key_value);
-int		is_newline(char *str);
-void	my_putstr_fd(char *str, int fd);
-void	process_pipe(t_data *d);
-int     pipe_func(t_data *data, int *fx, int fd, int idx);
+void				delete_last_newline(char **cmd);
+int					match_key(char *key_value, char *str);
+void				get_value(char *str, char *buf);
+int					findenv(t_data *data, char *buf);
+void				renewer_env(t_data *data, char *key, char *str, int size);
+int					delete_env(t_data *data, char *key);
+int					add_env(t_data *data, char *key_value);
+int					is_newline(char *str);
+void				my_putstr_fd(char *str, int fd);
+void				process_pipe(t_data *d);
+int					pipe_func(t_data *data, int *fx, int fd, int idx);
 
-void		ft_check_redirection(t_data *d);
-void		ft_check_env(t_data *d);
-void		ft_check_word(char *str, int *i);
-void		ft_check_argv(char *str);
-void		ft_check_pipe(char *str);
-void		ft_command(t_data *d, int *fd_std, int *fd_cmd, int pipe1);
+void				ft_check_redirection(t_data *d);
+void				ft_check_env(t_data *d);
+void				ft_check_word(char *str, int *i);
+void				ft_check_argv(char *str);
+void				ft_check_pipe(char *str);
+void				ft_command(t_data *d, int *fd_std, int *fd_cmd, int pipe1);
 
-int		is_exec_bin(t_data *data);
-int		is_exec_usr(t_data *data);
-int		is_builtin(t_data *data);
-void	process_builtin(t_data *data);
-void		ft_check_split(t_data *d, int idx);
+int					is_exec_bin(t_data *data);
+int					is_exec_usr(t_data *data);
+int					is_builtin(t_data *data);
+void				process_builtin(t_data *data);
+void				ft_check_split(t_data *d, int idx);
 
-void	porcess_echo(t_data *data);
-void	recover_std(t_data *d);
-void    process_pwd(t_data *data);
-void    process_env(t_data *data);
-void    process_cd(t_data *data);
-void    process_export(t_data *data);
-void    process_unset(t_data *data);
-int		is_exec_usr(t_data *data);
-int		is_exec_bin(t_data *data);
-int		is_builtin(t_data *data);
+void				porcess_echo(t_data *data);
+void				recover_std(t_data *d);
+void				process_pwd(t_data *data);
+void				process_env(t_data *data);
+void				process_cd(t_data *data);
+void				process_export(t_data *data);
+void				process_unset(t_data *data);
+int					is_exec_usr(t_data *data);
+int					is_exec_bin(t_data *data);
+int					is_builtin(t_data *data);
 #endif
