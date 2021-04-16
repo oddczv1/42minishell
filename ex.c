2f2f8c5a12540fc40e4441802f11e8943e1ffc3e
oddczv1 <youngrch@c6r3s4.42seoul.kr>
2021-04-16 20:41:10 +0900

    223232

--- ex.c
+++ ex.c
@@ -4,10 +4,8 @@
 #include <stdlib.h>
 #include <termcap.h>
 
-# define BACKSPACE 127
-# define LEFT_ARROW 4479771
-# define RIGHT_ARROW 4414235
-
+#include <string.h>
+#include "./libft/libft.h"
 
 //struct termios
 //{
@@ -19,106 +17,113 @@
 //    speed_t  c_ispeed;   /* input speed */
 //    speed_t  c_ospeed;   /* output speed */
 //};
-/*
+
+int	nbr_length(int n)
+{
+	int	i = 0;
+
+	if (n <= 0)
+		i++;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+int		putchar_tc(int tc)
+{
+	write(1, &tc, 1);
+	return (0);
+}
+
 int main()
 {
+	int c;
 	struct termios ti;
+	int col;
+	int max;
+	char *old_cmd;
+	
 	tcgetattr(0, &ti);
-	ti.c_lflag &= ~ICANON;//캐노니컬 입력 모드를 사용
-	ti.c_lflag &= ~ECHO;//입력을 다시 출력
+	ti.c_lflag &= (~ECHO);//입력을 다시 출력
+	ti.c_lflag &= (~ICANON);//캐노니컬 입력 모드를 사용
+	//ti.c_lflag &= (~ISIG); // 특수문자 무시
 	ti.c_cc[VMIN] = 1;
 	ti.c_cc[VTIME] = 0;
-}
-*/
-/*
-#include <stdlib.h>
-#include <stdio.h>
-
-#include <termios.h>
-#include <string.h>
-
-static struct termios stored_settings;
-void echo_off(void)
-{
-    struct termios new_settings;
-    tcgetattr(0, &stored_settings);
-    new_settings = stored_settings;
-    new_settings.c_lflag &= (~ECHO);
-    new_settings.c_lflag &= (~ICANON);
-    tcsetattr(0,TCSANOW, &new_settings);
-
-    return ;
-}
-
-void echo_on(void)
-{
-    tcsetattr(0, TCSANOW, &stored_settings);
-    return ;
-}
-
-void get_pass(char *pass)
-{
-    char buf;
-    int i=0;
-
-    printf("Passwd : ");
-    echo_off();
-    while ((buf=getc(stdin)) != '\n')
-    {
-        pass[i] = buf;
-        printf("%s", "*");
-        i ++;
-    }
-    echo_on();
-}
-
-int main()
-{
-    char pass[16];
-    memset(pass, 0x00, 16);
-    get_pass(pass);
-    printf("\nYour input : %s\n", pass);
+	old_cmd = 0;
+	tcsetattr(0, TCSANOW, &ti);
+	tgetent(NULL, "xterm");
+	char *buffer = (char *)malloc(1024);
+	while (1)
+	{
+  	  	write(1, ">>> ~% ", 7);
+		c = 0;
+		col = 0;
+		max = 0;
+    	while (read(0, &c, sizeof(c)) > 0)
+    	{
+			if (c ==  4479771) //left
+			{		
+				if (col > 0)
+				{
+					--col;
+					//tputs(tgoto(cm, col, row), 1, putchar_tc);
+					tputs(tgetstr("le", &buffer), 1, putchar_tc);				
+				}
+			}
+			else if(c ==  4414235)//right
+			{
+				if (col < max)
+				{
+					++col;
+					tputs(tgetstr("nd", &buffer), 1, putchar_tc);
+				}
+			}
+			else if (c == 127) //backspace
+			{	
+				if (col > 0)
+				{
+					--col;
+					--max;
+					tputs(tgetstr("le", &buffer), 1, putchar_tc);
+					tputs(tgetstr("dc", &buffer), 1, putchar_tc);
+				}
+			}
+			else if (c == 2117294875)
+			{
+				if (max > col && col >= 0)
+				{
+					--max;
+					tputs(tgetstr("dc", &buffer), 1, putchar_tc);
+				}
+			}
+			else if (c == 4348699 || c == 4283163)
+			{
+			}
+			else if (c == 9)
+			{
+			}
+			else if (c == 10)
+			{
+			//	tputs(tgoto(tgetstr("nd", &old_cmd), 7 + max, 0), 1, putchar_tc);			
+				break;
+			//	ft_putstr_fd(old_cmd[0],2);
+			}
+			else
+			{ 
+				//ft_putstr_fd(ft_itoa(c),2);
+				++col;
+				++max;
+				tputs(tgetstr("im", &buffer), 1, putchar_tc);
+				write(0, &c, 1);				
+			}
+			tputs(tgetstr("ei", &buffer), 1, putchar_tc);
+			c = 0;
+  		}
+		tputs(buffer, 1, putchar_tc);
+		write(1, "\n", 1);
+	}
     return 1;
 }
-*/
-/*
-#include <stdio.h>
-#include <termios.h>
-
-struct termios old_settings;
-int main()
-{
-    struct termios new_settings;
-    char buf[80];
-    tcgetattr(0, &old_settings);
-    new_settings = old_settings;
-    new_settings.c_lflag &= (~ISIG); // 특수문자 무시
-    tcsetattr(0, TCSANOW, &new_settings);
-
-    fgets(buf, 80, stdin);
-    fputs(buf, stdout);
-    tcsetattr(0, TCSANOW, &old_settings);
-}
-*/
-#include <stdio.h>
-#include <termios.h>
-
-struct termios old_settings;
-int main()
-{
-    struct termios new_settings;
-    char buf;
-
-    tcgetattr(0, &old_settings);
-    new_settings = old_settings;
-	new_settings.c_lflag &= (~ICANON);
-    tcsetattr(0, TCSANOW, &new_settings);
-
-    printf("youre OK (Y/N)? ");
-    buf=getc(stdin);
-	printf("\nyour input is %c\n", buf);
-    tcsetattr(0, TCSANOW, &old_settings);
-}
-
-
-
