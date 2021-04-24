95bec946aebe27226850aceb7fffe506e6d191fa
Youngrok Cho <youngrch@c6r8s8.42seoul.kr>
2021-04-25 04:04:21 +0900

    and redirectipn

---
+++ src/utils_for_free.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_for_free.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngrch <youngrch@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 02:51:01 by youngrch          #+#    #+#             */
+/*   Updated: 2021/04/25 02:51:23 by youngrch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void		ft_free(char **array)
+{
+	int i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+void		ft_cmd_free(t_data *d)
+{
+	int i;
+
+	i = 0;
+	while (i < d->max_idx)
+	{
+		if (d->cmd[i])
+			free(d->cmd[i]);
+		i++;
+	}
+	free(d->cmd);
+}
